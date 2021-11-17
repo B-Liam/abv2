8bdf6c77560288cf465d7e9526e55bbe20d65c2d
B-Liam <liam@oblbr.co.uk>
2021-11-17 10:42:38 +0000

    default page template added

---
+++ src/_data/vegdata.js
@@ -0,0 +1,12 @@
+const allVeg =  require('./varliam.json');
+const storeData = require('./varvegsuttons.json');
+
+//write a function to combine data from both the above
+
+//create an export of all the objects grouped into one big file - see video on how to do this
+
+module.exports = function() {
+    const newData = allVeg;
+    return newData;
+}
+
