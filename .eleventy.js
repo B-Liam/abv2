7386bfd4b05d23f229952cd6a3af57033a70f808
B-Liam <liam@oblbr.co.uk>
2023-01-05 14:43:55 +0000

    2023 first commit to ensure all changes are backed up

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,20 @@
 const embedEverything = require("eleventy-plugin-embed-everything");
+const markdownShortcode = require("eleventy-plugin-markdown-shortcode");
 
 module.exports = function(eleventyConfig) {
+    let markdownIt = require("markdown-it");
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true
+      };
+    eleventyConfig.setLibrary("md", markdownIt(options));
+    eleventyConfig.addShortcode("liamMark", function(text) {
+        var md = require('markdown-it')({html:true,breaks:true,linkify:true});
+        var result = md.render(text);
+            return result;
+      });
+    eleventyConfig.addPlugin(markdownShortcode);
     eleventyConfig.addPassthroughCopy("src/_redirects");
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/images");
